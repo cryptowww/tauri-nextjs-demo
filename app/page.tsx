da1fc61ab6d3c6d5c3c332186c40d9ab78810e51
James Kotling <3201920+cryptowww@users.noreply.github.com>
2024-06-24 21:41:21 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,48 @@
+import { invoke } from "@tauri-apps/api/tauri";
+import { listen } from "@tauri-apps/api/event";
+import { useEffect, useState } from "react";
+
+interface ProgressEventPayload {
+  progress: number;
+}
+
+interface ProgressEventProps {
+  payload: ProgressEventPayload;
+}
+
+export default function Home() {
+  const [busy, setBusy] = useState<boolean>(false);
+
+  useEffect(() => {
+    // listen what can Rust part tell us about
+    const unListen = listen("PROGRESS", (e: ProgressEventProps) => {
+      console.log(e.payload.progress);
+    });
+
+    return () => {
+      unListen.then((f) => f());
+    };
+  }, []);
+
+  return (
+    <div>
+      {!busy && (
+        <button
+          onClick={() => {
+            setBusy(true);
+            setTimeout(async () => {
+              const { appWindow } = await import("@tauri-apps/api/window");
+              // call Rust function, pass the window
+              await invoke("progress_tracker", {
+                window: appWindow,
+              });
+              setBusy(false);
+            }, 1000);
+          }}
+        >
+          Start Progress
+        </button>
+      )}
+    </div>
+  );
+}
