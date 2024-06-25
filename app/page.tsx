ecfc8630d8a9e8ddecebe2617d87122b91aa6926
James Kotling <3201920+cryptowww@users.noreply.github.com>
2024-06-25 14:36:07 +0800

    all demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import { invoke } from "@tauri-apps/api/tauri";
 import { listen } from "@tauri-apps/api/event";
 import { useEffect, useState } from "react";
+import { Button, Progress } from "antd";
 
 interface ProgressEventPayload {
   progress: number;
@@ -12,37 +15,59 @@ interface ProgressEventProps {
 
 export default function Home() {
   const [busy, setBusy] = useState<boolean>(false);
+  const [progress, setProgress] = useState<number>(0);
+  const [timeLabel, setTimeLabel] = useState<string>();
 
   useEffect(() => {
-    // listen what can Rust part tell us about
+    const timeIntervalId = setInterval(() => {
+      setTimeLabel(new Date().toLocaleTimeString());
+    }, 1000);
     const unListen = listen("PROGRESS", (e: ProgressEventProps) => {
-      console.log(e.payload.progress);
+      setProgress(e.payload.progress);
     });
 
     return () => {
+      clearInterval(timeIntervalId);
       unListen.then((f) => f());
     };
   }, []);
 
   return (
     <div>
-      {!busy && (
-        <button
-          onClick={() => {
-            setBusy(true);
-            setTimeout(async () => {
-              const { appWindow } = await import("@tauri-apps/api/window");
-              // call Rust function, pass the window
-              await invoke("progress_tracker", {
-                window: appWindow,
-              });
-              setBusy(false);
-            }, 1000);
-          }}
-        >
-          Start Progress
-        </button>
-      )}
+      <div style={{ position: "fixed", top: 20, left: 20 }}>{timeLabel}</div>
+      <div style={{ width: "70vw" }}>
+        <Progress percent={progress} />
+      </div>
+      <Button
+        type="primary"
+        disabled={busy}
+        onClick={() => {
+          setBusy(true);
+          setTimeout(async () => {
+            const { appWindow } = await import("@tauri-apps/api/window");
+            await invoke("progress_tracker", {
+              window: appWindow,
+            });
+            setBusy(false);
+          }, 1000);
+        }}
+      >
+        Start Progress
+      </Button>
+      {/* New code */}
+      <Button
+        type="primary"
+        disabled={!busy}
+        onClick={async () => {
+          const { appWindow } = await import("@tauri-apps/api/window");
+          await appWindow.emit("STOP");
+          setProgress(0);
+          setBusy(false);
+        }}
+      >
+        Stop Progress
+      </Button>
+      {/* / New code */}
     </div>
   );
 }
